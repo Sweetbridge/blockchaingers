61ed2a71d3adc6c865c22fc64260975e36d48c06
Josh Ransom <josh.bh.ransom@gmail.com>
2018-04-07 15:28:45 +0200

    logo: Add sweetbridge logo to main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Upload from './Upload'
+import config from './config'
 import './App.css';
 
 class App extends Component {
@@ -13,7 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={config.logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to ID Silo</h1>
         </header>
         <Upload />
